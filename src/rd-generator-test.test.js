83ff2c666579f1d8668cea121eb9dff0b0f8fa6c
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-09-03 17:07:48 -0300

    test2

---
+++ src/rd-generator-test.test.js
@@ -0,0 +1,11 @@
+describe('utils tests', () => {
+  it('baita teste', () => {
+    const date = new Date();
+    expect(date).toBeDefined();
+  });
+
+  it('baita teste2', () => {
+    const date = new Date();
+    expect(date).toBeDefined();
+  });
+});
